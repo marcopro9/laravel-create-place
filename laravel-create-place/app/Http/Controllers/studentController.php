7cb69082c2bd66d9bfd83ef1f5827dc35997ccc9
Marco Prosperi <marcoprosperi@outlook.com>
2020-09-01 17:44:59 +0200

    primo commit

---
+++ laravel-create-place/app/Http/Controllers/studentController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class studentController extends Controller
+{
+    public function view(){
+      dd('ciao sono il metodo student');
+    }
+}
